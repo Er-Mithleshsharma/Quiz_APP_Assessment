70fa73759f45a2a28c5c22e48aa36da1769e4717
Mithlesh Sharma <itsyour.rohan@gmail.com>
2024-03-31 21:39:45 +0530

    responsiveness

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -14,7 +14,7 @@ const Result = () => {
     })
   return (
     <div>
-        <h1 className='text-5xl bg-black w-full text-center h-28 md:h-24 pt-4 text-orange-400 font-sans font-bold'>EssayChecker Quiz</h1>
+        <h1 className='text-2xl md:text-5xl bg-black w-full text-center h-20 md:h-24 pt-4 text-orange-400 font-sans font-bold'>EssayChecker Quiz</h1>
     <div className="max-w-4xl w-full p-8 bg-white rounded-lg shadow-lg mx-auto">
         <h1 className='text-xl font-bold'>{name} Your Result is :</h1>
         <h1 className='font-bold text-center text-5xl'>{`${ans.length}/10`}</h1>
